8734c198ac0ca0653467804ae064a4faf4866554
Joel Meuleman <j-jolabola@live.com>
2025-04-03 14:04:45 -0600

    Starting work on playback container.

--- src/lib/PbContainer/PbContainer.svelte
+++ src/lib/PbContainer/PbContainer.svelte
@@ -1,3 +1,8 @@
-<div class="col-span-6 bg-gray-600 h-48">
-  <h2 class="text-white text-xl p-4">Presentation Bar</h2>
+<script lang="ts">
+  import PbContainerElem from "./PbContainerElem.svelte";
+</script>
+
+<div class="col-span-6 bg-gray-600 h-64">
+  <h2 class="text-white text-xl p-4">Mix Presentation Playback & Export</h2>
+  <PbContainerElem></PbContainerElem>
 </div>
---
+++ src/lib/PbContainer/PbContainerElem.svelte
@@ -0,0 +1,12 @@
+<div class="flex justify-around border p-3" id="playback-mix-presentation">
+  <button
+    class="bg-amber-100 hover:bg-green-100"
+    id="playback-mp-play-btn"
+    type="button">Play</button
+  >
+  <span id="playback-mp-name">Playback Mix Presentation</span>
+  <input id="playback-mp-slider" type="range" />
+  <button class="bg-amber-100 hover:bg-green-100" id="playback-mp-export"
+    >Export</button
+  >
+</div>
