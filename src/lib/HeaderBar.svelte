9598bc261c9baeece1d6611a39bbc19b1d0ef4dc
Joel Meuleman <j-jolabola@live.com>
2025-04-03 15:31:15 -0600

    Headerbar.:

---
+++ src/lib/HeaderBar.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="col-span-5 row-span-1 grid grid-cols-5">
+  <h2 class="col-span-1 p-2 font-semibold text-xl">Audio Elements</h2>
+  <h2 class="col-span-1 p-2 font-semibold text-xl">Mix Presentations</h2>
+  <h2 class="col-span-1 p-2 font-semibold text-xl col-start-5">Export</h2>
+</div>
