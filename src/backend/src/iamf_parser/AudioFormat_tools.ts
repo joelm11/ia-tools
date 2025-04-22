8d5d8da181fbc55cc3cc8ccafe3eb239b74a699c
Joel Meuleman <j-jolabola@live.com>
2025-04-22 17:03:27 -0600

    WOrking on formatting MP MD.

---
+++ src/backend/src/iamf_parser/AudioFormat_tools.ts
@@ -0,0 +1,174 @@
+import { AudioChFormat } from "src/@types/AudioFormats";
+import { ChannelLabel } from "./protoc/audio_frame";
+import { LoudspeakerLayout } from "./protoc/audio_element";
+
+export function getChannelLabels(chFormat: AudioChFormat) {
+  switch (chFormat) {
+    case AudioChFormat.MONO:
+      return [ChannelLabel.CHANNEL_LABEL_MONO];
+    case AudioChFormat.STEREO:
+      return [ChannelLabel.CHANNEL_LABEL_L_2, ChannelLabel.CHANNEL_LABEL_R_2];
+    case AudioChFormat.K3P1P2:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_3,
+        ChannelLabel.CHANNEL_LABEL_R_3,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LTF_3,
+        ChannelLabel.CHANNEL_LABEL_RTF_3,
+      ];
+    case AudioChFormat.K5P1:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_5,
+        ChannelLabel.CHANNEL_LABEL_R_5,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LS_5,
+        ChannelLabel.CHANNEL_LABEL_RS_5,
+      ];
+    case AudioChFormat.K5P1P2:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_5,
+        ChannelLabel.CHANNEL_LABEL_R_5,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LS_5,
+        ChannelLabel.CHANNEL_LABEL_RS_5,
+        ChannelLabel.CHANNEL_LABEL_LTF_2,
+        ChannelLabel.CHANNEL_LABEL_RTF_2,
+      ];
+    case AudioChFormat.K5P1P4:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_5,
+        ChannelLabel.CHANNEL_LABEL_R_5,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LS_5,
+        ChannelLabel.CHANNEL_LABEL_RS_5,
+        ChannelLabel.CHANNEL_LABEL_LTF_4,
+        ChannelLabel.CHANNEL_LABEL_RTF_4,
+        ChannelLabel.CHANNEL_LABEL_LTB_4,
+        ChannelLabel.CHANNEL_LABEL_RTB_4,
+      ];
+    case AudioChFormat.K7P1:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_7,
+        ChannelLabel.CHANNEL_LABEL_R_7,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LSS_7,
+        ChannelLabel.CHANNEL_LABEL_RSS_7,
+        ChannelLabel.CHANNEL_LABEL_LRS_7,
+        ChannelLabel.CHANNEL_LABEL_RRS_7,
+      ];
+    case AudioChFormat.K7P1P2:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_7,
+        ChannelLabel.CHANNEL_LABEL_R_7,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LSS_7,
+        ChannelLabel.CHANNEL_LABEL_RSS_7,
+        ChannelLabel.CHANNEL_LABEL_LRS_7,
+        ChannelLabel.CHANNEL_LABEL_RRS_7,
+        ChannelLabel.CHANNEL_LABEL_LTF_2,
+        ChannelLabel.CHANNEL_LABEL_RTF_2,
+      ];
+    case AudioChFormat.K7P1P4:
+      return [
+        ChannelLabel.CHANNEL_LABEL_L_7,
+        ChannelLabel.CHANNEL_LABEL_R_7,
+        ChannelLabel.CHANNEL_LABEL_CENTRE,
+        ChannelLabel.CHANNEL_LABEL_LFE,
+        ChannelLabel.CHANNEL_LABEL_LSS_7,
+        ChannelLabel.CHANNEL_LABEL_RSS_7,
+        ChannelLabel.CHANNEL_LABEL_LRS_7,
+        ChannelLabel.CHANNEL_LABEL_RRS_7,
+        ChannelLabel.CHANNEL_LABEL_LTF_4,
+        ChannelLabel.CHANNEL_LABEL_RTF_4,
+        ChannelLabel.CHANNEL_LABEL_LTB_4,
+        ChannelLabel.CHANNEL_LABEL_RTB_4,
+      ];
+    default:
+      throw new Error(`getChSet(): Unknown channel format: ${chFormat}`);
+  }
+}
+
+export function getCoupledChannelCount(chFormat: AudioChFormat): number {
+  switch (chFormat) {
+    case AudioChFormat.MONO:
+      return 0;
+    case AudioChFormat.STEREO:
+      return 1;
+    case AudioChFormat.K3P1P2:
+      return 2;
+    case AudioChFormat.K5P1:
+      return 2;
+    case AudioChFormat.K5P1P2:
+      return 3;
+    case AudioChFormat.K5P1P4:
+      return 4;
+    case AudioChFormat.K7P1:
+      return 3;
+    case AudioChFormat.K7P1P2:
+      return 4;
+    case AudioChFormat.K7P1P4:
+      return 5;
+    default:
+      throw new Error(
+        `getCoupledChannelCount(): Unknown channel format: ${chFormat}`
+      );
+  }
+}
+
+export function getChannelCountRaw(chFormat: AudioChFormat): number {
+  switch (chFormat) {
+    case AudioChFormat.MONO:
+      return 1;
+    case AudioChFormat.STEREO:
+      return 2;
+    case AudioChFormat.K3P1P2:
+      return 6;
+    case AudioChFormat.K5P1:
+      return 6;
+    case AudioChFormat.K5P1P2:
+      return 8;
+    case AudioChFormat.K5P1P4:
+      return 10;
+    case AudioChFormat.K7P1:
+      return 8;
+    case AudioChFormat.K7P1P2:
+      return 10;
+    case AudioChFormat.K7P1P4:
+      return 12;
+    default:
+      throw new Error(
+        `getChannelCountRaw(): Unknown channel format: ${chFormat}`
+      );
+  }
+}
+
+export function getIAMFLayout(chFormat: AudioChFormat): any {
+  switch (chFormat) {
+    case AudioChFormat.MONO:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_MONO;
+    case AudioChFormat.STEREO:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_STEREO;
+    case AudioChFormat.K3P1P2:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_3_1_2_CH;
+    case AudioChFormat.K5P1:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_5_1_CH;
+    case AudioChFormat.K5P1P2:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_5_1_2_CH;
+    case AudioChFormat.K5P1P4:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_5_1_4_CH;
+    case AudioChFormat.K7P1:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_7_1_CH;
+    case AudioChFormat.K7P1P2:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_7_1_2_CH;
+    case AudioChFormat.K7P1P4:
+      return LoudspeakerLayout.LOUDSPEAKER_LAYOUT_7_1_4_CH;
+    default:
+      throw new Error(`getIAMFLayout(): Unknown channel format: ${chFormat}`);
+  }
+}
--- src/backend/src/iamf_parser/iamf_converter.ts
+++ src/backend/src/iamf_parser/iamf_converter.ts
@@ -21,20 +21,69 @@ import {
 import { TemporalDelimiterObuMetadata } from "./protoc/temporal_delimiter";
 import { UserMetadata } from "./protoc/user_metadata";
 import type { MixPresentationBase } from "src/@types/MixPresentation";
+import type { AudioChFormat } from "src/@types/AudioFormats";
+import type { AudioElementBase } from "src/@types/AudioElement";
+import {
+  getChannelCountRaw,
+  getCoupledChannelCount,
+  getChannelLabels,
+  getIAMFLayout,
+} from "./AudioFormat_tools";
+
+interface AudioElementMetadata extends AudioElementBase {
+  idInt: number;
+  channelLabels: ChannelLabel[];
+}
+
+const CODEC_CONFIG_ID = 200;
+const CODEC_SR = 48000;
+const CODEC_BIT_DEPTH = 16;
 
 export function payloadToIAMF(mixPresentations: MixPresentationBase[]) {
+  // Safety check in case of single mix presentation.
+  if (!Array.isArray(mixPresentations)) {
+    mixPresentations = [mixPresentations];
+  }
+
+  const audioElements = augmentAudioElementMetadata(mixPresentations);
+
   let metadata = UserMetadata.create();
 
-  addDefaultMetadata(metadata);
-  addAudioFileData(metadata);
-  addAudioElementData(metadata);
-  addMixPresentationData(metadata);
-
+  addDefaultMetadata(mixPresentations, metadata);
+  addAudioFileData(audioElements, metadata);
+  addAudioElementData(audioElements, metadata);
+  addMixPresentationData(mixPresentations, metadata);
   // Write the metadata to a JSON file.
   metadataToTextProto(metadata);
 }
 
-function addDefaultMetadata(metadata: UserMetadata) {
+function augmentAudioElementMetadata(
+  mixPresentations: MixPresentationBase[]
+): AudioElementMetadata[] {
+  const audioElements: AudioElementMetadata[] = [];
+  for (const mixPresentation of mixPresentations) {
+    for (const audioElement of mixPresentation.audioElements) {
+      const elementId = uuidToNumber(audioElement.id);
+      const channelLabels = getChannelLabels(audioElement.audioChFormat);
+      audioElements.push({
+        ...audioElement,
+        idInt: elementId,
+        channelLabels: channelLabels,
+      });
+    }
+  }
+  return audioElements;
+}
+
+function uuidToNumber(uuid: string): number {
+  const hexString = uuid.replace(/-/g, "");
+  return Number(`0x${hexString.slice(-16)}`);
+}
+
+function addDefaultMetadata(
+  mixPresentations: MixPresentationBase[],
+  metadata: UserMetadata
+) {
   // Test vector field
   metadata.testVectorMetadata = TestVectorMetadata.create({ isValid: true });
 
@@ -49,15 +98,15 @@ function addDefaultMetadata(metadata: UserMetadata) {
   // Codec config field
   metadata.codecConfigMetadata.push(
     CodecConfigObuMetadata.create({
-      codecConfigId: 200,
+      codecConfigId: CODEC_CONFIG_ID,
       // Hardcoding these for now as params are independent of input files.
       codecConfig: {
         codecId: CodecId.CODEC_ID_LPCM,
         numSamplesPerFrame: 512,
         decoderConfigLpcm: {
           sampleFormatFlags: LpcmFormatFlags.LPCM_LITTLE_ENDIAN,
-          sampleSize: 16,
-          sampleRate: 48000,
+          sampleSize: CODEC_BIT_DEPTH,
+          sampleRate: CODEC_SR,
         },
       },
     })
@@ -71,94 +120,119 @@ function addDefaultMetadata(metadata: UserMetadata) {
   );
 }
 
-function addAudioFileData(metadata: UserMetadata) {
-  metadata.audioFrameMetadata.push(
-    AudioFrameObuMetadata.create({
-      wavFilename: "audio.wav", // TODO
-      samplesToTrimAtEndIncludesPadding: false,
-      samplesToTrimAtStartIncludesCodecDelay: false,
-      samplesToTrimAtEnd: 0,
-      samplesToTrimAtStart: 0,
-      audioElementId: 100, // TODO
-      channelMetadatas: [
-        // TODO
-        { channelId: 0, channelLabel: ChannelLabel.CHANNEL_LABEL_A_0 },
-      ],
-    })
-  );
+function addAudioFileData(
+  audioElements: AudioElementMetadata[],
+  metadata: UserMetadata
+) {
+  for (const element of audioElements) {
+    metadata.audioFrameMetadata.push(
+      AudioFrameObuMetadata.create({
+        wavFilename: element.id,
+        samplesToTrimAtEndIncludesPadding: false,
+        samplesToTrimAtStartIncludesCodecDelay: false,
+        samplesToTrimAtEnd: 0,
+        samplesToTrimAtStart: 0,
+        audioElementId: element.idInt,
+        channelMetadatas: element.channelLabels.map((label, index) => {
+          return {
+            channelId: index,
+            channelLabel: label,
+          };
+        }),
+      })
+    );
+  }
 }
 
-function addAudioElementData(metadata: UserMetadata) {
-  metadata.audioElementMetadata.push(
-    AudioElementObuMetadata.create({
-      audioElementId: 100,
-      audioElementType: AudioElementType.AUDIO_ELEMENT_CHANNEL_BASED,
-      codecConfigId: 200, // TODO this needs to change with file data for the element.
-      numSubstreams: 1, // TODO
-      audioSubstreamIds: [0], // TODO
-      numParameters: 0,
-      scalableChannelLayoutConfig: {
-        numLayers: 1,
-        channelAudioLayerConfigs: [
-          {
-            loudspeakerLayout: 0, // TODO
-            outputGainIsPresentFlag: 0, // TODO
-            reconGainIsPresentFlag: 0, // TODO
-            substreamCount: 1,
-            coupledSubstreamCount: 0,
-          },
-        ],
-      },
-    })
-  );
-}
-
-function addMixPresentationData(metadata: UserMetadata) {
-  metadata.mixPresentationMetadata.push(
-    MixPresentationObuMetadata.create({
-      mixPresentationId: 0, // TODO
-      countLabel: 1,
-      annotationsLanguage: ["en-us"], // TODO
-      localizedPresentationAnnotations: ["MP Name"], // TODO
-      subMixes: [
-        {
-          audioElements: [
+function addAudioElementData(
+  audioElements: AudioElementMetadata[],
+  metadata: UserMetadata
+) {
+  for (const element of audioElements) {
+    const numSubstreams =
+      getChannelCountRaw(element.audioChFormat) -
+      getCoupledChannelCount(element.audioChFormat);
+    metadata.audioElementMetadata.push(
+      AudioElementObuMetadata.create({
+        audioElementId: element.idInt,
+        audioElementType: AudioElementType.AUDIO_ELEMENT_CHANNEL_BASED,
+        codecConfigId: CODEC_CONFIG_ID,
+        numSubstreams: numSubstreams,
+        audioSubstreamIds: Array.from(
+          { length: numSubstreams },
+          (_, index) => index
+        ),
+        numParameters: 0,
+        scalableChannelLayoutConfig: {
+          numLayers: 1,
+          channelAudioLayerConfigs: [
             {
-              audioElementId: 300,
-              localizedElementAnnotations: ["ah"],
-              renderingConfig: {
-                headphonesRenderingMode:
-                  HeadPhonesRenderingMode.HEADPHONES_RENDERING_MODE_BINAURAL,
-              },
-              // TODO?
-              elementMixGain: {
-                paramDefinition: {
-                  parameterId: 999,
-                  parameterRate: 48000,
-                  paramDefinitionMode: true,
-                },
-                defaultMixGain: 0,
-              },
-            }, // TODO
-          ],
-          outputMixGain: {
-            paramDefinition: {
-              parameterId: 998,
-              parameterRate: 48000,
-              paramDefinitionMode: true,
+              loudspeakerLayout: getIAMFLayout(element.audioChFormat),
+              outputGainIsPresentFlag: 0, // TODO(?)
+              reconGainIsPresentFlag: 0, // TODO(?)
+              substreamCount: numSubstreams,
+              coupledSubstreamCount: getCoupledChannelCount(
+                element.audioChFormat
+              ),
             },
-            defaultMixGain: 0,
-          },
-          // Let's see if we can get away without loudness information.
-          // Otherwise, TODO.
+          ],
         },
-      ], // TODO
-    })
-  );
+      })
+    );
+  }
+}
+
+function addMixPresentationData(
+  mixPresentations: MixPresentationBase[],
+  metadata: UserMetadata
+) {
+  for (const mixPresentation of mixPresentations) {
+    metadata.mixPresentationMetadata.push(
+      MixPresentationObuMetadata.create({
+        mixPresentationId: 0, // TODO
+        countLabel: 1,
+        annotationsLanguage: ["en-us"], // TODO
+        localizedPresentationAnnotations: ["MP Name"], // TODO
+        subMixes: [
+          {
+            audioElements: [
+              {
+                audioElementId: 300,
+                localizedElementAnnotations: ["ah"],
+                renderingConfig: {
+                  headphonesRenderingMode:
+                    HeadPhonesRenderingMode.HEADPHONES_RENDERING_MODE_BINAURAL,
+                },
+                // TODO?
+                elementMixGain: {
+                  paramDefinition: {
+                    parameterId: 999,
+                    parameterRate: 48000,
+                    paramDefinitionMode: true,
+                  },
+                  defaultMixGain: 0,
+                },
+              }, // TODO
+            ],
+            outputMixGain: {
+              paramDefinition: {
+                parameterId: 998,
+                parameterRate: 48000,
+                paramDefinitionMode: true,
+              },
+              defaultMixGain: 0,
+            },
+            // Let's see if we can get away without loudness information.
+            // Otherwise, TODO.
+          },
+        ], // TODO
+      })
+    );
+  }
 }
 
 function metadataToTextProto(metadata: UserMetadata) {
   const foo = UserMetadata.toJSON(metadata);
-  console.log("-- Constructed MD --");
-  console.log(foo);
+  // console.log("-- Constructed MD --");
+  // console.log(foo);
 }
