67f8444f7891124144a84b444cb8fbbbc807c8ec
Joel Meuleman <j-jolabola@live.com>
2025-04-21 19:51:57 -0600

    Begin work on structure IAMF MD from payload MD.

--- src/backend/src/iamf_parser/iamf_converter.ts
+++ src/backend/src/iamf_parser/iamf_converter.ts
@@ -1,6 +1,27 @@
+import { TestVectorMetadata } from "./protoc/test_vector_metadata";
+import { AudioFrameObuMetadata } from "./protoc/audio_frame";
+import { IASequenceHeaderObuMetadata } from "./protoc/ia_sequence_header";
+import { CodecConfigObuMetadata } from "./protoc/codec_config";
+import { AudioElementObuMetadata } from "./protoc/audio_element";
+import { MixPresentationObuMetadata } from "./protoc/mix_presentation";
+import { TemporalDelimiterObuMetadata } from "./protoc/temporal_delimiter";
+import { UserMetadata } from "./protoc/user_metadata";
+
 export function payloadToIAMF(payload: any) {
-  // Add audio file data.
-  // Add audio element data.
-  // Add mix presentation data.
-  // Add codec data?
+  let metadata = UserMetadata.create();
+
+  addDefaultMetadata();
+  addAudioFileData();
+  addAudioElementData();
+  addMixPresentationData();
+  // Print the metadata to check its validity.
+  console.log("Metadata: ", metadata);
 }
+
+function addDefaultMetadata() {}
+
+function addAudioFileData() {}
+
+function addAudioElementData() {}
+
+function addMixPresentationData() {}
