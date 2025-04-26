fbb3e577ebde287f18a448019330fb3fb85708d0
Joel Meuleman <j-jolabola@live.com>
2025-04-26 15:23:51 -0600

    Vite config.

---
+++ backend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import * as path from "path";
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "src/@lib": path.resolve(__dirname, "./src/lib"),
+      "src/@types": path.resolve(__dirname, "../common/types"),
+    },
+  },
+});
