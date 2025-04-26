52033f586b550e465e978b4fe8ac80dcdaa69528
Joel Meuleman <j-jolabola@live.com>
2025-04-26 15:23:39 -0600

    Working on storage unit tests with vitest.

--- backend/src/storage/storage.ts
+++ backend/src/storage/storage.ts
@@ -1,10 +1,16 @@
-export type StorageReturn =
-  | { success: true; url: string }
-  | { success: false; error: string }
-  | { success: boolean };
+export type StorageReturn = {
+  success: boolean;
+  url?: string;
+  error?: string;
+};
 
+/**
+ * We use an ID to identify a file.
+ * This ID is generated indepdendently of the interface.
+ * This ID acts as the handle to the file.
+ */
 export interface Storage {
-  create(file: Express.Multer.File): Promise<StorageReturn>;
+  create(file: Express.Multer.File, fileID: string): Promise<StorageReturn>;
   exists(fileID: string): Promise<StorageReturn>;
   delete(fileID: string): Promise<StorageReturn>;
 }
