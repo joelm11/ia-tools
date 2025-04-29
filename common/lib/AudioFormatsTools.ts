b2d4408ea4dca17ea0eb1292295751d9c6ace5c8
Joel Meuleman <j-jolabola@live.com>
2025-04-28 18:48:14 -0600

    Moved some utils.

---
+++ common/lib/AudioFormatsTools.ts
@@ -0,0 +1,84 @@
+import { AudioChFormat } from "../types/AudioFormats";
+
+// Checks channel number and channel mask to determine the audio format
+export function audioFormatFromChannels(
+  numChannels: number,
+  channelMask?: number
+): AudioChFormat {
+  switch (numChannels) {
+    case 1:
+      return AudioChFormat.MONO;
+    case 2:
+      return AudioChFormat.STEREO;
+    case 6:
+      if (channelMask === 0x3f) {
+        return AudioChFormat.K5P1;
+      } else {
+        return AudioChFormat.K3P1P2;
+      }
+    case 8:
+      if (channelMask === 0x503f) {
+        return AudioChFormat.K5P1P2;
+      } else {
+        return AudioChFormat.K7P1;
+      }
+    case 10:
+      if (channelMask === 0x2d03f) {
+        return AudioChFormat.K5P1P4;
+      } else {
+        return AudioChFormat.K7P1P2;
+      }
+    case 12:
+      return AudioChFormat.K7P1P4;
+  }
+  return AudioChFormat.NONE;
+}
+
+// Checks channel number and returns possible audio format candidates
+export function audioFormatsFromChannels(
+  numChannels: number,
+  channelMask?: number
+): AudioChFormat[] {
+  switch (numChannels) {
+    case 1:
+      return [AudioChFormat.MONO];
+    case 2:
+      return [AudioChFormat.STEREO];
+    case 6:
+      return [AudioChFormat.K5P1, AudioChFormat.K3P1P2];
+    case 8:
+      return [AudioChFormat.K7P1, AudioChFormat.K5P1P2];
+    case 10:
+      return [AudioChFormat.K7P1P2, AudioChFormat.K5P1P4];
+    case 12:
+      return [AudioChFormat.K7P1P4];
+  }
+  return [AudioChFormat.NONE];
+}
+
+export function getChannelCountRaw(chFormat: AudioChFormat): number {
+  switch (chFormat) {
+    case AudioChFormat.MONO:
+      return 1;
+    case AudioChFormat.STEREO:
+      return 2;
+    case AudioChFormat.K3P1P2:
+      return 6;
+    case AudioChFormat.K5P1:
+      return 6;
+    case AudioChFormat.K5P1P2:
+      return 8;
+    case AudioChFormat.K5P1P4:
+      return 10;
+    case AudioChFormat.K7P1:
+      return 8;
+    case AudioChFormat.K7P1P2:
+      return 10;
+    case AudioChFormat.K7P1P4:
+      return 12;
+    default:
+      throw new Error(
+        `getChannelCountRaw(): Unknown channel format: ${chFormat}`
+      );
+  }
+}
