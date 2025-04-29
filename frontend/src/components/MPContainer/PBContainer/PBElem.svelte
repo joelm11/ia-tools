e4b9a24efa2148dbbd17f7dd902a978287dd9959
Joel Meuleman <j-jolabola@live.com>
2025-04-29 11:26:25 -0600

    Redesign of pbelem.

--- frontend/src/components/MPContainer/PBContainer/PBElem.svelte
+++ frontend/src/components/MPContainer/PBContainer/PBElem.svelte
@@ -16,30 +16,24 @@
 </script>
 
 <div
-  class="grid grid-cols-6 bg-app border border-ae-card-background rounded-md gap-2 p-2"
+  class="grid grid-cols-2 bg-app border border-ae-card-background rounded-md gap-2 p-2"
 >
-  <!-- Mix Play and Mix Volume -->
-  <div id="play-and-volume" class="col-span-1 h-24 flex items-center pl-3">
+  <div id="play-and-volume" class="col-span-1 grid grid-cols-5 p-2">
+    <div
+      id="wave-viz col-span-5"
+      class="col-span-5 h-20 mb-2 rounded-md bg-ae-card-background border border-card-s-text"
+    ></div>
     <button
+      id="play-pause"
+      class="col-span-2 mt-2 flex items-center justify-center bg-mp-card-t-background rounded-full w-14 h-14 hover:bg-mp-card-t-background-dark"
       onclick={handlePlayPause}
-      class="p-2 bg-mp-card-t-background hover:bg-mp-card-t-background-dark rounded-full w-12 h-12 flex items-center justify-center"
-      aria-label="Play Mix Presentation"
+      aria-label="Play/Pause Mix Presentation"
     >
-      <i class="fas fa-play text-2xl"></i>
-    </button>
+      <i class="fas fa-play text-2xl text-mp-card-t-text"> </i></button
+    >
   </div>
-
-  <!-- Placeholder for waveform -->
   <div
-    class="col-span-5 h-24 bg-card-s-text text-center flex items-center justify-center"
-  >
-    Placeholder for Waveform
-  </div>
-
-  <!-- Render Audio Elements -->
-  <div class="col-span-3 flex flex-col gap-2">
-    {#each mixPresentation.audioElements as audioElement}
-      <PbElemAe {audioElement}></PbElemAe>
-    {/each}
-  </div>
+    id="mp-audio-elements"
+    class="col-span-1 h-24 flex items-center p-2"
+  ></div>
 </div>
