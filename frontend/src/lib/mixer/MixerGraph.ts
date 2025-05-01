55d97eba56512a0923e3c7e70b067cee34284947
Joel Meuleman <j-jolabola@live.com>
2025-05-01 12:15:01 -0600

    Working on downmix impl.

--- frontend/src/lib/mixer/MixerGraph.ts
+++ frontend/src/lib/mixer/MixerGraph.ts
@@ -1,9 +1,11 @@
 import type { Mixer } from "./Mixer.svelte";
 import {
-  surroundNodes,
-  topNodes,
+  surroundNodeList,
+  topNodeList,
   type MixerGraphNode,
 } from "./MixerGraphNodes";
+import Queue from "./Queue";
+import { Matrix } from "mathjs";
 
 export enum SurroundType {
   S7,
@@ -27,4 +29,53 @@ type SurroundFormat = {
 export function constructGraph(
   inputFormat: SurroundFormat,
   outputFormat: SurroundFormat
-) {}
+) {
+  const surroundNodes = queueNodes(
+    inputFormat.surround,
+    outputFormat.surround,
+    surroundNodeList
+  );
+
+  // Add top nodes if the output format has top channels.
+  let topNodes: Queue<MixerGraphNode> | undefined;
+  if (outputFormat.top && inputFormat.top) {
+    topNodes = queueNodes(inputFormat.top, outputFormat.top, topNodeList);
+  }
+}
+
+function queueNodes(
+  input: SurroundType | TopType,
+  output: SurroundType | TopType,
+  nodeList: MixerGraphNode[]
+): Queue<MixerGraphNode> {
+  let nodes = new Queue<MixerGraphNode>();
+
+  const inputType = input;
+  const outputType = output;
+  let currType = inputType;
+  while (currType !== outputType) {
+    const node = nodeList[currType];
+    nodes.enqueue(node);
+    currType++;
+  }
+
+  return nodes;
+}
+
+function downmixMatFromNodes(
+  numInputChannels: number,
+  surroundNodes: Queue<MixerGraphNode>,
+  topNodes?: Queue<MixerGraphNode>
+) {
+  while (!surroundNodes.isEmpty() || topNodes?.isEmpty()) {
+    // Construct gain matrix for this step.
+    const surroundNode = surroundNodes.dequeue();
+    const topNode = topNodes?.dequeue();
+    if (surroundNode && topNode) {
+    } else if (surroundNode) {
+    } else {
+    }
+  }
+}
+
+function gainMatFromNodes() {}
--- frontend/src/lib/mixer/MixerGraphNodes.ts
+++ frontend/src/lib/mixer/MixerGraphNodes.ts
@@ -1,13 +1,13 @@
 import { SurroundType, TopType } from "./MixerGraph";
 
 type ChannelMapItem = {
+  srcCh: number;
   gain: number;
-  destCh: number | number[];
 };
-// Type that maps an input channel idx to an object with
-// a gain and a dest channel idx
+
+// Type that maps a destination channel idx to an array of ChannelMapItems
 export type ChannelMap = {
-  [key: number]: ChannelMapItem;
+  [key: number]: ChannelMapItem[];
 };
 
 export type MixerGraphNode = {
@@ -25,72 +25,91 @@ const alpha = 0.5,
 const S7Node: MixerGraphNode = {
   label: SurroundType.S7,
   conversions: {
-    0: { gain: 1, destCh: 0 },
-    1: { gain: 1, destCh: 1 },
-    2: { gain: 1, destCh: 2 },
-    3: { gain: 1, destCh: 3 },
-    4: { gain: alpha, destCh: 4 },
-    5: { gain: alpha, destCh: 5 },
-    6: { gain: beta, destCh: 4 },
-    7: { gain: beta, destCh: 5 },
+    0: [{ srcCh: 0, gain: 1 }],
+    1: [{ srcCh: 1, gain: 1 }],
+    2: [{ srcCh: 2, gain: 1 }],
+    3: [{ srcCh: 3, gain: 1 }],
+    4: [
+      { srcCh: 4, gain: alpha },
+      { srcCh: 6, gain: beta },
+    ],
+    5: [
+      { srcCh: 5, gain: alpha },
+      { srcCh: 7, gain: beta },
+    ],
   },
 };
 
 const S5Node: MixerGraphNode = {
   label: SurroundType.S5,
   conversions: {
-    0: { gain: 1, destCh: 0 },
-    1: { gain: 1, destCh: 1 },
-    2: { gain: 1, destCh: 2 },
-    3: { gain: 1, destCh: 3 },
-    4: { gain: delta, destCh: [0, 4] },
-    5: { gain: delta, destCh: [1, 5] },
+    0: [
+      { srcCh: 0, gain: 1 },
+      { srcCh: 4, gain: delta },
+    ],
+    1: [
+      { srcCh: 1, gain: 1 },
+      { srcCh: 5, gain: delta },
+    ],
+    2: [{ srcCh: 2, gain: 1 }],
+    3: [{ srcCh: 3, gain: 1 }],
+    4: [{ srcCh: 4, gain: delta }],
+    5: [{ srcCh: 5, gain: delta }],
   },
 };
 
 const S3Node: MixerGraphNode = {
   label: SurroundType.S3,
   conversions: {
-    0: { gain: 1, destCh: 0 },
-    1: { gain: 1, destCh: 1 },
-    2: { gain: 0.71, destCh: [2, 3] },
-    3: { gain: 0, destCh: 0 },
+    0: [{ srcCh: 0, gain: 1 }],
+    1: [{ srcCh: 1, gain: 1 }],
+    2: [{ srcCh: 2, gain: 0.71 }],
+    3: [{ srcCh: 2, gain: 0.71 }],
   },
 };
 
 const S2Node: MixerGraphNode = {
   label: SurroundType.S2,
   conversions: {
-    0: { gain: 0.35, destCh: 0 },
-    1: { gain: 0.35, destCh: 1 },
+    0: [{ srcCh: 0, gain: 0.35 }],
+    1: [{ srcCh: 1, gain: 0.35 }],
   },
 };
 
 const T4Node: MixerGraphNode = {
   label: TopType.T4,
   conversions: {
-    0: { gain: 1, destCh: 0 },
-    1: { gain: 1, destCh: 1 },
-    2: { gain: gamma, destCh: 0 },
-    3: { gain: gamma, destCh: 1 },
+    0: [
+      { srcCh: 0, gain: 1 },
+      { srcCh: 2, gain: gamma },
+    ],
+    1: [
+      { srcCh: 1, gain: 1 },
+      { srcCh: 3, gain: gamma },
+    ],
   },
 };
 
 const T2Node: MixerGraphNode = {
   label: TopType.T2,
   conversions: {
-    0: { gain: 1, destCh: 0 },
-    1: { gain: 1, destCh: 1 },
+    0: [{ srcCh: 0, gain: 1 }],
+    1: [{ srcCh: 1, gain: 1 }],
   },
 };
 
 const TF2Node: MixerGraphNode = {
   label: TopType.TF2,
   conversions: {
-    0: { gain: 1, destCh: 0 },
-    1: { gain: 1, destCh: 1 },
+    0: [{ srcCh: 0, gain: 1 }],
+    1: [{ srcCh: 1, gain: 1 }],
   },
 };
 
-export const surroundNodes: MixerGraphNode[] = [S7Node, S5Node, S3Node, S2Node];
-export const topNodes: MixerGraphNode[] = [T4Node, T2Node, TF2Node];
+export const surroundNodeList: MixerGraphNode[] = [
+  S7Node,
+  S5Node,
+  S3Node,
+  S2Node,
+];
+export const topNodeList: MixerGraphNode[] = [T4Node, T2Node, TF2Node];
